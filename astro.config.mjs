a0ea3a2de29d01b24e73d34c52f82aa821c0cd67
edfii <edith.fink@gmx.net>
2025-07-24 22:09:08 +0200

    fix style path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -9,7 +8,7 @@ export default defineConfig({
   outDir: 'dist',
   site: 'https://edfii.github.io/mindTools',
   vite: {
-    plugins: [tailwindcss(), react()],
+    plugins: [react()],
   },
   integrations: [react({
       include: ['**/react/*'],
